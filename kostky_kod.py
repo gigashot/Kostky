dbfd86d4fcea4d934f87139c3b618322ac8c66b3
gigashot <147614954+gigashot@users.noreply.github.com>
2024-01-23 21:15:36 +0100

    1.3

--- kostky_kod.py
+++ kostky_kod.py
@@ -1,18 +1,25 @@
 import random
 
 def main_fce():
-    def roll_dice():
-        return [random.randint(1, 6) for _ in range(6)]
+    def roll_dice(manual_input=False):
+        if manual_input:
+            dice = [int(input("Zadejte hodnotu pro kostku {}: ".format(i + 1))) for i in range(6)]
+        else:
+            dice = [random.randint(1, 6) for _ in range(6)]
+        return dice
 
-    def calculate_score(dice):
+    def calculate_score(dice): 
+        # Hledá všechny kombinace
         score = 0
-
-        # Hledá všechny speciální kombinace
+        
         if dice == [1, 2, 3, 4, 5, 6]:
-            return score == 1500
+            score += 1500
 
         elif all(x == 1 for x in dice):
-            score += 2 ** (len(dice) - 1) * 1000
+            if dice.count(1) == 5:
+                score += 4000
+            else:
+                score += 2 ** (len(dice) - 1) * 1000
 
         elif all(x == dice[0] for x in dice):
             if dice[0] == 1:
@@ -22,7 +29,6 @@ def main_fce():
 
         elif len(set(dice)) == 3 and all(dice.count(x) == 2 for x in set(dice)):
             score += 1000
-
 
         else:
             # Hledá samotné 1 a 5
@@ -41,10 +47,12 @@ def main_fce():
     total_score = 0
 
     while True:
-        input("zmáčkněte Enter pro hod kostkou...")
-
-        # hod 6 kostkami
-        dice = roll_dice()
+        manual_input = input("Chcete generovat hod kostkou, nebo házet vlastními kostakmi (generuj/vlastní): ").lower()
+        if manual_input == "vlastní":
+            dice = roll_dice(manual_input=True)
+        else:
+            input("Zmáčkněte Enter pro hod kostkou...")
+            dice = roll_dice()
 
         print("Kosty: {}".format(dice))
 
@@ -64,3 +72,4 @@ def main_fce():
 
 # Vyvolávání celého kódu
 main_fce()
+
